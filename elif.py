ac7fa878831065869a43f5d3280b76902eba4593
Tyler Morris <pianomanzero@gmail.com>
2013-09-09 12:23:08 -0700

    elif script to illustrate if/elif/else structure

---
+++ elif.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#example of using if/elif/else
+
+people = int(raw_input("enter number of people: "))
+cars = int(raw_input("enter number of cars: "))
+buses = int(raw_input("enter number of busses; "))
+
+if cars > people:
+  print "We should take more cars"
+elif cars < people:
+  print "We should not take more cars"
+else:
+  print "We can't decide what to do!!!"
+
+if buses > cars:
+  print "That's too many busses"
+elif buses < cars:
+  print "Maybe we should use the bus..."
+else:
+  print "We still can't decide what to do!!!"
+
+if people > buses:
+  print "Alright, let's just take the buss!"
+else:
+  print "Fine, we're just gonna stay home, cause we just can't decide what to do!!!"
+
