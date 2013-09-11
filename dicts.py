ff026067a209c3b6d8936c52105332abcc1e9a3f
Tyler Morris <pianomanzero@gmail.com>
2013-09-11 14:39:56 -0700

    adding dictionary scripts, list scripts

---
+++ dicts.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#script to illustrate use of dictionaries
+
+#initiate dict
+stuff = {'name':'Zed', 'age':36, 'height': 6*12+2}
+
+print "stuff: ", stuff
+
+print "stuff['name']: ", stuff['name']
+print "stuff['age']: ", stuff['age']
+print "stuff['height']: ", stuff['height']
+print "set a city name to add to the stuff dictionary:"
+stuff['city'] = raw_input("> ")
+print "stuff['city']: ", stuff['city']
+
+#dicts can also be addressed with numbered indexes instead of strings
+stuff[1] = "Boom!"
+stuff[2] = "Wow!"
+
+print "stuff[1]: ", stuff[1]
+print "stuff[2]: ", stuff[2]
+print "stuff dict: ", stuff
+
+print "You can delete entries from dictionaries as well:"
+
+del stuff['city']
+del stuff[1]
+del stuff[2]
+print stuff
---
+++ funWithLists.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import time
+
+ten_things = "Apples Oranges Crows Telephones Lights Sugar"
+
+print "Ten things: ", ten_things
+
+time.sleep(1)
+
+print "Um, wait, that's not ten things...", time.sleep(1), "let's fix that.."
+
+#using the split list function, passing delimiter
+stuff = ten_things.split(' ')
+more_stuff = ["day","night","song","frisbee","corn","banana","girl","boy"]
+
+while len(stuff) != 10:
+#use the pop list function to pop an antry from a list into another var
+  next_one = more_stuff.pop()
+  print "Adding: ", next_one
+  #take popped value dropped into next_one and append to stuff list using append list function
+  stuff.append(next_one)
+  print "There's now %d items in the stuff list" % len(stuff)
+
+print "There we go: ", stuff
+
+print "Let's do some more things with stuff"
+
+time.sleep(1)
+
+print "stuff[1]: ", stuff[1]
+
+time.sleep(1)
+
+print "stuff[-1]: ", stuff[-1]
+print "using stuff.pop() ", stuff.pop()
+print ' '.join(stuff)
+print '#'.join(stuff[3:5])
+
