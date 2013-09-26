8aa3cb866aee63b97a2c62c6c4c9e6092c6536c0
Tyler Morris <pianomanzero@gmail.com>
2013-09-26 11:21:22 -0700

    corrections to smb.py

--- smb.py
+++ smb.py
@@ -19,84 +19,82 @@ import isi.gconfig as gcfg
 
 
 def processArgs():
-        usage = "usage: %prog [options]"
-        parser = optparse.OptionParser(usage=usage)
-        parser.add_option('--verbrose','-v', dest='verbrose', 
-                          action='store_true', default=False)
-
-        (options, args) = parser.parse_args()
-        return (options, args)
+  usage = "usage: %prog [options]"
+  parser = optparse.OptionParser(usage=usage)
+  parser.add_option('--verbrose','-v', dest='verbrose',action='store_true', default=False)
+  (options, args) = parser.parse_args()
+  return (options, args)
 
 
 class GlobalStrings:
-        # global strings to key from having stupid typos
-        REGPATH_SRV_SHARES = "registry.Services.lwio.Parameters.Drivers.srv.shares._multi_sz"
-        REGPATH_SRV_SHARE_ACLS = "registry.Services.lwio.Parameters.Drivers.srv.shares.security._binary"
-        REGPATH_ONEFS_SHARES = "registry.Services.lwio.Parameters.Drivers.onefs.shares._key"
+  # global strings to key from having stupid typos
+  REGPATH_SRV_SHARES = "registry.Services.lwio.Parameters.Drivers.srv.shares._multi_sz"
+  REGPATH_SRV_SHARE_ACLS = "registry.Services.lwio.Parameters.Drivers.srv.shares.security._binary"
+  REGPATH_ONEFS_SHARES = "registry.Services.lwio.Parameters.Drivers.onefs.shares._key"
 
-        nameKey   = '_name'
+  nameKey   = '_name'
 
 g = GlobalStrings()
 
 # 
 def expandString(str):
-        str = str.replace(" ","%20")
-        str = str.replace("-","%2d")
-        return str.replace("$","%24") 
+  str = str.replace(" ","%20")
+  str = str.replace("-","%2d")
+  return str.replace("$","%24") 
 
                        
 def run():
-        ShareDict = dict()
+  ShareDict = dict()
 
-        cfg = gcfg.open(gcfg.gcfg_main)
-        ctx = cfg.ctx_new()
-        allShares = ctx.read(g.REGPATH_SRV_SHARES)
+  cfg = gcfg.open(gcfg.gcfg_main)
+  ctx = cfg.ctx_new()
+  allShares = ctx.read(g.REGPATH_SRV_SHARES)
 
-        for share in allShares:
-               if options.verbrose==True:
-                       print "found share ", share[g.nameKey] 
-               ShareDict[share[g.nameKey]] = "SRV"
+  for share in allShares:
+    if options.verbrose==True:
+      print "found share ", share[g.nameKey]
+  ShareDict[share[g.nameKey]] = "SRV"
 
-        # Now look at the onefs share parts and we must
-        # match 1:1 for the data to be okay
-        allShares = ctx.read(g.REGPATH_ONEFS_SHARES)
+  # Now look at the onefs share parts and we must
+  # match 1:1 for the data to be okay
+  allShares = ctx.read(g.REGPATH_ONEFS_SHARES)
 
-        for share in allShares:
-               if share[g.nameKey] not in ShareDict:
-                       if options.verbrose==True:
-                               print "deleting ophaned share",expandString(share[g.nameKey])
-                       ctx.delete(g.REGPATH_ONEFS_SHARES+"."+expandString(share[g.nameKey]))
-               else:
-                       del ShareDict[share[g.nameKey]]
+  for share in allShares:
+    if share[g.nameKey] not in ShareDict:
+      if options.verbrose==True:
+        print "deleting ophaned share",expandString(share[g.nameKey])
+      ctx.delete(g.REGPATH_ONEFS_SHARES+"."+expandString(share[g.nameKey]))
+    else:
+      del ShareDict[share[g.nameKey]]
 
         # Now we have to remove the leftover shares
-        for shareName in ShareDict:
-                if options.verbrose==True:
-                       print "removing removing",expandString(shareName)
-               ctx.delete(g.REGPATH_SRV_SHARES+"."+expandString(shareName))
+    for shareName in ShareDict:
+      if options.verbrose==True:
+        print "removing removing",expandString(shareName)
+      ctx.delete(g.REGPATH_SRV_SHARES+"."+expandString(shareName))
 
-        # For next pass a valid share list is needed
-        ShareDict = dict()
-        allShares = ctx.read(g.REGPATH_SRV_SHARES)
+    # For next pass a valid share list is needed
+    ShareDict = dict()
+    allShares = ctx.read(g.REGPATH_SRV_SHARES)
 
-        for share in allShares:
-               if options.verbrose==True:
-                       print "Found complete share ", share[g.nameKey] 
-               ShareDict[share[g.nameKey]] = "SRV"
+    for share in allShares:
+      if options.verbrose==True:
+        print "Found complete share ", share[g.nameKey] 
+      ShareDict[share[g.nameKey]] = "SRV"
 
-        # check for any abandoned ACLs
-        allShares = ctx.read(g.REGPATH_SRV_SHARE_ACLS)
+    # check for any abandoned ACLs
+    allShares = ctx.read(g.REGPATH_SRV_SHARE_ACLS)
 
-        for share in allShares:
-               if share[g.nameKey] not in ShareDict:
-                       if options.verbrose==True: 
-                               print "Removing abandoned ACLs for share", share[g.nameKey]
-                       ctx.delete(g.REGPATH_SRV_SHARE_ACLS+"."+expandString(share[g.nameKey]))
+    for share in allShares:
+      if share[g.nameKey] not in ShareDict:
+        if options.verbrose==True: 
+          print "Removing abandoned ACLs for share", share[g.nameKey]
+        ctx.delete(g.REGPATH_SRV_SHARE_ACLS+"."+expandString(share[g.nameKey]))
 
-        ctx.commit()  # needed to actually updated the file. 
+    ctx.commit()  # needed to actually updated the file. 
 
 
-#
+  #
 (options, args) = processArgs() 
 #print "opts: ", options
 #print "args: ", args
